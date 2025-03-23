8223e22c994be1916a679252cff96b3c59c21723
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-03-23 23:45:24 +0200

    oops i should have just read the docs lmfao this was easy

--- src/app/cve/[cve]/layout.tsx
+++ src/app/cve/[cve]/layout.tsx
@@ -1,26 +1,28 @@
-export async function generateMetadata({
-  params,
-  _searchParams,
-}: {
-  params: { cve: string };
-  _searchParams: { [key: string]: string | string[] | undefined };
-}) {
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ cve: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const { cve } = await params;
+
   // Fetch data server-side; note that you might want to handle errors/404s here too.
-  const res = await fetch(`https://cveawg.mitre.org/api/cve/${params.cve}`, {
+  const res = await fetch(`https://cveawg.mitre.org/api/cve/${cve}`, {
     cache: "no-store",
   });
   const cveData = await res.json();
 
   // Customize these properties based on your CVE API response structure
   return {
-    title: params.cve
-      ? `${params.cve} | KioydioLabs CVEThingy`
-      : "KioydioLabs CVEThingy",
+    title: cve ? `${cve} | KioydioLabs CVEThingy` : "KioydioLabs CVEThingy",
     description: `${cveData?.containers?.cna?.descriptions?.[0]?.value}`,
     openGraph: {
-      title: params.cve
-        ? `${params.cve} | KioydioLabs CVEThingy`
-        : "KioydioLabs CVEThingy",
+      title: cve ? `${cve} | KioydioLabs CVEThingy` : "KioydioLabs CVEThingy",
     },
   };
 }
