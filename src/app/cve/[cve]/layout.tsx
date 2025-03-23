e35792c9088254f27dcbb155b3df8cf1563954b9
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-03-24 00:49:14 +0200

    i literally just give up

--- src/app/cve/[cve]/layout.tsx
+++ src/app/cve/[cve]/layout.tsx
@@ -1,27 +1,7 @@
-export async function generateMetadata({
-  params,
-}: {
-  params: { cve: string };
-}) {
-  const res = await fetch(`https://cveawg.mitre.org/api/cve/${params.cve}`, {
-    cache: "no-store",
-  });
-  const cveData = await res.json();
-
-  // Basic metadata
-  return {
-    title: `${params.cve} | KioydioLabs CVEThingy`,
-    description:
-      cveData?.containers?.cna?.descriptions?.[0]?.value || "CVE details",
-  };
-}
-
 export default function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { cve: string };
 }>) {
   return (
     <div className="flex flex-col items-center justify-center gap-4 w-full">
